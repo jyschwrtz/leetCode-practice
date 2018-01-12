8b659d5826794db331389ec250b7c74921e1287d
jyschwrtz <jyschwrtz@gmail.com>
2018-01-11 16:17:35 -0800

    refactor palindrome_number for more efficient solution

--- palindrome_number.js
+++ palindrome_number.js
@@ -1,13 +1,15 @@
 var isPalindrome = function(x) {
-    if (x < 0) {
-        return false
-    }
-    let str = ("" + x).split("");
-
-    a = str.slice(0).reverse();
-    if (a.join("") == str.join("")) {
-        return true;
-    } else {
+    if ( x < 0 || (x !== 0 && x % 10 === 0)) {
         return false;
     }
+    let temp = x;
+    let reverse = 0;
+    while (temp !== 0) {
+        let nextNum = temp % 10;
+        reverse = reverse * 10 + nextNum;
+        temp = Math.floor(temp / 10);
+    }
+    return reverse === x;
 };
+
+console.log(isPalindrome(1));
