15095960fb84770a256c39ca074e39391d21b46c
jyschwrtz <jyschwrtz@gmail.com>
2018-01-22 13:33:12 -0800

    refactor counting_bits to use dynamic programming

--- counting_bits.js
+++ counting_bits.js
@@ -3,9 +3,14 @@
  * @return {number[]}
  */
 var countBits = function(num) {
-  let result = [];
-  for (let i = 0; i <= num; i++) {
-    result.push(binaryCount(i));
+  let result = [0];
+  let offset = 1;
+  for (let i = 1; i <= num; i++) {
+    if (i === offset * 2) {
+      offset = offset * 2;
+    }
+    let count = result[i - offset] + 1;
+    result.push(count);
   }
   return result;
 };
@@ -26,19 +31,19 @@ var countBits = function(num) {
 //   return result;
 // };
 
-const binaryCount = (num) => {
-  let currentNum = num;
-  let result = 0;
-  let power = 1;
-  while (currentNum > 0) {
-    if (currentNum % (Math.pow(2, power)) !== 0) {
-      result += 1;
-      currentNum = currentNum - Math.pow(2, power - 1);
-    }
-    power += 1;
-  }
-  return result;
-};
+// const binaryCount = (num) => {
+//   let currentNum = num;
+//   let result = 0;
+//   let power = 1;
+//   while (currentNum > 0) {
+//     if (currentNum % (Math.pow(2, power)) !== 0) {
+//       result += 1;
+//       currentNum = currentNum - Math.pow(2, power - 1);
+//     }
+//     power += 1;
+//   }
+//   return result;
+// };
 
 
 console.log(countBits(7));
