52038adef955388b44b322f7b40c52035b4fe67f
jyschwrtz <jyschwrtz@gmail.com>
2018-01-24 10:41:06 -0800

    find_duplicates ready for testing

--- find_duplicates.js
+++ find_duplicates.js
@@ -12,5 +12,16 @@
 // [2,3]
 
 var findDuplicates = function(nums) {
-  
+  let counts = {};
+  let result = [];
+  nums.forEach( num => {
+    if (counts[num]) {
+      result.push(num);
+    } else {
+      counts[num] = true;
+    }
+  });
+  return result;
 };
+
+console.log(findDuplicates([4,3,2,7,8,2,3,1]), [2,3]);
