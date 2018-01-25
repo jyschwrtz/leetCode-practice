be09006ecb355ac3e08a738dbf49bd66bdabd349
jyschwrtz <jyschwrtz@gmail.com>
2018-01-25 10:40:23 -0500

    my_calendar_three beginning

--- single_non_duplicate.js
+++ single_non_duplicate.js
@@ -1,15 +1,3 @@
-// Given a sorted array consisting of only integers where every element appears twice except for one element which appears once. Find this single element that appears only once.
-//
-// Example 1:
-//        [0,1,2,3,*4,5,6,7,8]
-// Input: [1,1,2,3,*3,4,4,8,8]
-// Output: 2
-// Example 2:
-//        [0,1,2,*3, 4, 5, 6]
-// Input: [3,3,7,*7,10,11,11]
-// Output: 10
-// Note: Your solution should run in O(log n) time and O(1) space.
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -22,7 +10,7 @@ var singleNonDuplicate = function(nums) {
   let midIdx = (nums.length - 1) / 2;
     if (nums[midIdx] === nums[midIdx + 1]) {
       if (midIdx % 2 === 0) { // even
-        return singleNonDuplicate(nums.slice(midIdx + 1));
+        return singleNonDuplicate(nums.slice(midIdx + 2));
       } else {
         return singleNonDuplicate(nums.slice(0, midIdx));
       }
