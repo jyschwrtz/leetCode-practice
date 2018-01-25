f8556c551f892df3d78c9fa2627014e60e9500c5
jyschwrtz <jyschwrtz@gmail.com>
2018-01-25 17:19:06 -0500

    single_number with bit xor

--- single_number.js
+++ single_number.js
@@ -2,19 +2,29 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    let counts = {};
-    for (let i = 0; i < nums.length; i++) {
-        let current = nums[i];
-        if (counts[current]) {
-            counts[current] = false;
-        } else {
-            counts[current] = true;
-        }
-    }
-    for (let i = 0; i < nums.length; i++) {
-        if (counts[nums[i]]) {
-          return nums[i];
-        }
-    }
+const singleNumber = (nums) => {
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    result = result ^ nums[i];
+  }
+  return result;
 };
+
+
+
+// var singleNumber = function(nums) {
+//     let counts = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         let current = nums[i];
+//         if (counts[current]) {
+//             counts[current] = false;
+//         } else {
+//             counts[current] = true;
+//         }
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         if (counts[nums[i]]) {
+//           return nums[i];
+//         }
+//     }
+// };
